9f9b8a6904bde412ae9e40e1072540b21a3047c8
aleksandar9999a <aleksandar9999a@gmail.com>
2020-09-16 13:51:29 +0300

    Added Constellation

---
+++ src/components/Constellation.tsx
@@ -0,0 +1,73 @@
+import ExF, { Component, CustomElement, Ref } from 'exf-ts';
+import constellation_config from '../config/constellationConfig';
+import { IConstellationConfig } from '../interfaces/interfaces';
+import Star from './Star';
+
+function createStar(context: CanvasRenderingContext2D, width: number, height: number) {
+	return new Star(context, width, height);
+}
+
+@CustomElement({
+	selector: 'exf-constellation'
+})
+export class Constellation extends Component {
+	@Ref({ id: 'constellation' }) canvas!: HTMLCanvasElement;
+	private context!: CanvasRenderingContext2D | null;
+	private constellation_config!: IConstellationConfig;
+
+	onCreate() {
+		this.context = this.canvas.getContext("2d");
+		this.constellation_config = constellation_config;
+		this.canvas.width = this.constellation_config.width;
+		this.canvas.height = this.constellation_config.height;
+		this.init();
+	}
+
+	private setContext() {
+		if (!this.context) { return; }
+		this.context.fillStyle = this.constellation_config.star.color;
+		this.context.strokeStyle = this.constellation_config.line.color;
+		this.context.lineWidth = this.constellation_config.line.width;
+	};
+
+	private loop(callback: Function) {
+		callback()
+		requestAnimationFrame(this.loop.bind(this, callback));
+	};
+
+	private subscribeForResize(star: Star) {
+		window.addEventListener('resize', () => {
+			this.canvas.width = window.innerWidth - 20;
+			this.canvas.height = window.innerHeight;
+			this.setContext();
+			star.setWindowSize(window.innerWidth, window.innerHeight);
+		})
+	}
+
+	init() {
+		this.setContext();
+		const star = createStar(this.context as CanvasRenderingContext2D, this.canvas.width, this.canvas.height);
+		star.create();
+		this.subscribeForResize(star);
+		this.loop(star.animate.bind(star));
+		this.loop(star.line.bind(star));
+	};
+
+	stylize() {
+		return (
+			<style>
+				canvas {
+					{ 
+						position: 'absolute',
+						left: '0px',
+						top: '0px'
+					}
+				}
+			</style>
+		)
+	}
+
+	render() {
+		return <canvas id="constellation"></canvas>
+	}
+}
---
+++ src/components/Star.tsx
@@ -0,0 +1,100 @@
+import constellation_config from "../config/constellationConfig";
+import { IStarCoords } from "../interfaces/interfaces";
+
+export default class Star {
+    private width: number;
+    private height: number;
+    private context: CanvasRenderingContext2D | null;
+    private stars: IStarCoords[] = [];
+    private constellation_config = constellation_config;
+
+    constructor(context: CanvasRenderingContext2D, width: number, height: number) {
+        this.context = context;
+        this.width = width;
+        this.height = height;
+    }
+
+    setWindowSize(width: number, height: number) {
+        this.width = width;
+        this.height = height;
+    }
+
+    private generateRandomCoords() {
+        const x = Math.random() * this.width;
+        const y = Math.random() * this.height;
+        const vx = (this.constellation_config.velocity - (Math.random() * 0.5));
+        const vy = (this.constellation_config.velocity - (Math.random() * 0.5));
+        const radius = this.constellation_config.star.randomWidth ? (Math.random() * this.constellation_config.star.width) : this.constellation_config.star.width;
+        return { x, y, vx, vy, radius };
+    }
+
+    create() {
+        if (!this.context) { return; }
+        for (let i = 0; i < this.constellation_config.length; i++) {
+            const star = this.generateRandomCoords();
+            this.createCustomStar(star);
+            this.stars.push(star)
+        }
+    }
+
+    private createCustomStar({ x, y, radius }: { x: number, y: number, radius: number }) {
+        if (!this.context) { return; }
+        this.context.beginPath();
+        this.context.arc(x, y, radius, 0, Math.PI * 2, false);
+        this.context.fill();
+    }
+
+    private increaseCoords(star: IStarCoords) {
+        if (star.y < 0 || star.y > this.height) {
+            star.vy = -star.vy;
+        } else if (star.x < 0 || star.x > this.width) {
+            star.vx = -star.vx;
+        }
+        star.x += star.vx;
+        star.y += star.vy;
+        return star;
+    }
+
+    animate() {
+        if (!this.context) { return; }
+        this.context.clearRect(0, 0, this.width, this.height);
+        this.stars.map(this.increaseCoords.bind(this)).forEach(this.createCustomStar.bind(this));
+    }
+
+    private isValidDistance(iStar: IStarCoords, jStar: IStarCoords) {
+        return (iStar.x - jStar.x) < this.constellation_config.distance && (iStar.y - jStar.y) < this.constellation_config.distance && (iStar.x - jStar.x) > -this.constellation_config.distance && (iStar.y - jStar.y) > -this.constellation_config.distance;
+    }
+
+    private isValidRadius(iStar: IStarCoords, x: number, y: number) {
+        return (iStar.x - x) < this.constellation_config.radius && (iStar.y - y) < this.constellation_config.radius && (iStar.x - x) > -this.constellation_config.radius && (iStar.y - y) > -this.constellation_config.radius;
+    }
+
+    private writeLine(iStar: IStarCoords, jStar: IStarCoords) {
+        if (!this.context) { return; }
+        this.context.beginPath();
+        this.context.moveTo(iStar.x, iStar.y);
+        this.context.lineTo(jStar.x, jStar.y);
+        this.context.stroke();
+        this.context.closePath();
+    }
+
+    private initWriting(starOne: IStarCoords, starTwo: IStarCoords) {
+        if (
+            this.context
+            && (
+                this.isValidDistance(starOne, starTwo)
+                && (
+                    this.isValidRadius(starOne, this.width * 0.1, this.height * 0.3)
+                    || this.isValidRadius(starOne, this.width * 0.8, this.height * 0.6)
+                )
+            )
+        ) {
+            this.writeLine(starOne, starTwo);
+        }
+    }
+
+    line() {
+        if (!this.context) { return; }
+        this.stars.forEach(starOne => this.stars.forEach(starTwo => this.initWriting(starOne, starTwo)));
+    }
+}
