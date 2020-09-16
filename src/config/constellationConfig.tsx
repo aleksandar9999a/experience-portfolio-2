3605ebdbad9b9ad2f7c9fe106aa530198e3177d8
aleksandar9999a <aleksandar9999a@gmail.com>
2020-09-16 13:51:04 +0300

    Added Constellation config

---
+++ src/config/constellationConfig.tsx
@@ -0,0 +1,20 @@
+const constellation_config = {
+    star: {
+        color: '#08fdd8',
+        width: 1,
+        randomWidth: false
+    },
+    line: {
+        color: '#08fdd8',
+        width: 0.05,
+        randomWidth: true
+    },
+    width: window.innerWidth - 20,
+    height: window.innerHeight,
+    velocity: 0.2,
+    length: window.innerWidth < 500 ? 20 : 150,
+    distance: 80,
+    radius: 200
+}
+
+export default constellation_config
