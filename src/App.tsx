4c62de161a01d3da4d2ce2b58aa8f33a7c67b129
aleksandar9999a <aleksandar9999a@gmail.com>
2020-09-16 13:52:10 +0300

    Added new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
 import ExF, { Component, CustomElement } from 'exf-ts';
+import './App.css';
 
 @CustomElement({
 	selector: 'exf-app'
 })
 export class App extends Component {
 
+	stylize() {
+		return (
+			<style>
+				.wrapper {
+                    {
+                        'background-position': 'center',
+                        'background-repeat': 'no-repeat',
+                        'background-size': 'cover',
+                        'background-color': '#1d1d1d',
+                        'position': 'relative',
+                        'min-height': '100vh'
+                    }
+                }
+			</style>
+		)
+	}
+
 	render() {
 		return (
-			<div>
-				<h1>ExF</h1>
+			<div className="wrapper">
+				<exf-constellation />
+				<exf-overlay />
 			</div>
 		)
 	}
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,25 @@
 import 'core-js';
+import './interfaces/interfaces';
 import { ExFModule } from 'exf-ts';
 import { App } from './App';
+import { Navbar } from './components/Navbar';
+import { Overlay } from './components/Overlay';
+import { Constellation } from './components/Constellation';
+import { NavbarTab } from './components/NavTab';
+import { Logo } from './components/Logo';
+
 
 ExFModule({
 	components: [
-		App
+		Overlay,
+		App,
+		Navbar,
+		Constellation,
+        NavbarTab,
+        Logo
 	],
 	bootstraps: [
 		App
 	],
 	root: 'root'
 })
-
