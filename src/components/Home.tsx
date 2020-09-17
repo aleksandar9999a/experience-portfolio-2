9c1961468a5cd48353dc537bc6f75b99462b0d6f
aleksandar9999a <aleksandar9999a@gmail.com>
2020-09-17 15:45:18 +0300

    Added Home

---
+++ src/components/Home.tsx
@@ -0,0 +1,48 @@
+import ExF, { Component, CustomElement } from 'exf-ts';
+
+@CustomElement({
+	selector: 'exf-hero'
+})
+export class Hero extends Component {
+
+	stylize() {
+		return (
+			<style>
+                
+			</style>
+		)
+	}
+
+	render() {
+		return (
+			<div className="hero">
+                home<br />
+                home<br />
+                home<br />
+                home<br />
+                home<br />
+                home<br />
+                home<br />
+                home<br />
+                home<br />
+                home<br />
+                home<br />
+                home<br />
+                home<br />
+                home<br />
+                home<br />
+                home<br />
+                home<br />
+                home<br />
+                home<br />
+                home
+                home
+                home
+                home
+                home
+                home
+                home
+			</div>
+		)
+	}
+}
