60ca30b3827310fd72995bcc59be11adfe583a36
aleksandar9999a <aleksandar9999a@gmail.com>
2020-09-17 15:45:31 +0300

    Import new components

--- src/index.tsx
+++ src/index.tsx
@@ -7,19 +7,29 @@ import { Overlay } from './components/Overlay';
 import { Constellation } from './components/Constellation';
 import { NavbarTab } from './components/NavTab';
 import { Logo } from './components/Logo';
+import { Hero } from './components/Home';
+import { Footer } from './components/Footer';
+import { Route } from './Router/Route';
+import { RouterLink } from './Router/RouterLink';
+import { Router } from './Router/Router';
 
 
 ExFModule({
-	components: [
-		Overlay,
-		App,
-		Navbar,
-		Constellation,
+    components: [
+        Overlay,
+        App,
+        Navbar,
+        Constellation,
         NavbarTab,
-        Logo
-	],
-	bootstraps: [
-		App
-	],
-	root: 'root'
+        Logo,
+        Hero,
+        Footer,
+        Route,
+        RouterLink,
+        Router
+    ],
+    bootstraps: [
+        App
+    ],
+    root: 'root'
 })
