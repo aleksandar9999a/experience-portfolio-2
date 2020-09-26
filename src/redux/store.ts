bf7b532bb4ffd3c7232855923f6374e2b26db14e
aleksandar9999a <aleksandar9999a@gmail.com>
2020-09-26 16:16:13 +0300

    added support for redux extension

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,21 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import { allReducers } from './reducers';
 import middlewares from './../sagas/middlewares';
 
-const sagaMiddleware = createSagaMiddleware()
+const sagaMiddleware = createSagaMiddleware();
 
-export const store = createStore(allReducers, applyMiddleware(sagaMiddleware));
+const composeEnhancers =
+    typeof window === 'object' &&
+        (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+        (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        }) : compose;
+
+const enhancer = composeEnhancers(
+    applyMiddleware(sagaMiddleware),
+);
+
+export const store = createStore(allReducers, enhancer);
 
 middlewares.forEach(middleware => {
     sagaMiddleware.run(middleware);
