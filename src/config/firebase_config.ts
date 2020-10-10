6b6d6c1799a689b3272d29685f307e0c77dd8002
aleksandar9999a <aleksandar9999a@gmail.com>
2020-10-10 20:29:44 +0300

    remove config

--- src/config/firebase_config.ts
+++ src/config/firebase_config.ts
@@ -1,12 +1,12 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyByX7STdHRxpCZlOGCxWE2oHAXAEPY3PBI",
-    authDomain: "experience-portfolio-2.firebaseapp.com",
-    databaseURL: "https://experience-portfolio-2.firebaseio.com",
-    projectId: "experience-portfolio-2",
-    storageBucket: "experience-portfolio-2.appspot.com",
-    messagingSenderId: "459779908486",
-    appId: "1:459779908486:web:077d0c15bed0d327f50207",
-    measurementId: "G-M086ER6LST"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
-export const defaultUser = 'KG7x3G3eS8bIIB4OX6mlvfi0FaH3';
+export const defaultUser = '';
